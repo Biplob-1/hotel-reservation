4599609ec3c6121fa1ee7ad4087a91fa02bf086a
Biplob Sarkar <ramjansarkar3899@gmail.com>
2024-05-11 19:11:04 +0600

    privat route add

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,6 +1,16 @@
-const PrivateRoute = () => {
-    return(
-        <div></div>
-    )
+import { useContext } from "react";
+import { AuthContext } from "../Providers/AuthProvider";
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({children}) => {
+    const {user, loading} = useContext(AuthContext);
+    if (loading) {
+        return <span className="loading loading-infinity loading-lg"></span>
+    }
+    
+    if (user) {
+        return children;
+    }
+    return <Navigate to={'/login'}></Navigate>
 };
 export default PrivateRoute;
